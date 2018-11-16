2a96bd46383208ea18a89be1648168dbd372c37e
Abhishek Kumar <skylvr1289@users.noreply.github.com>
2018-11-16 16:49:19 +0530

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,23 +1,7 @@
-CREATE TABLE public.test1 (
-	id int8 NULL,
-	created_date timestamp NULL,
-	claass varchar(255) NULL,
-	fh float8 NULL,
-	ft float8 NULL,
-	mgfr varchar(255) NULL,
-	"month" int8 NULL,
-	rg float8 NULL,
-	rt float8 NULL,
-	serial_number varchar(255) NULL,
-	"year" int8 NULL,
-	gage_type varchar(255) NULL,
-	model varchar(255) NULL,
-	part_number varchar(255) NULL,
-	size_no int8 NULL DEFAULT 0,
-	"version" float8 NULL,
-	is_current bool NULL
-)
-WITH (
-	OIDS=FALSE
-) ;
-
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
